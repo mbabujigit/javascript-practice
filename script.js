c757e27f68a0b0f8a49b65182e8e408c6207bf52
mbabujigit <155733743+mbabujigit@users.noreply.github.com>
2024-01-31 20:37:03 -0500

    Javascript practice updated Version

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-fetch("https://jsonplaceholder.typicode.com/users") 
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        document.getElementById("name").innerHTML = data[0].name;   
-        document.getElementById("address").innerHTML = data[0].address.city;
-    })
+//Function Declaration
+function generateOTP() {
+    var otp = Math.floor(1000 + Math.random() * 900000);
+    return otp.toString().padStart(6, "0");
+}
+
+//Function Expression
+var generateOTP2 = function () {
+    console.log(a);
+};
+
+generateOTP();
+var a='Babuji';
+//Anonymous Function
+//IIFE
+(function () {
+    var a='Mannamma';
+    var otp = Math.floor(1000 + Math.random() * 900000);
+    console.log(otp.toString().padStart(6, "0")); //otp.toString().padStart(6, "0");
+    console.log(a);
+})();
+console.log(a);
+
+//hoisting
+console.log(generateOTP());
+console.log(generateOTP2());
+
+//falsy values  0, "", undefined, null, NaN 
