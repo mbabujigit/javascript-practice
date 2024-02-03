a6bbe6b61c173e022979d7a4e504ff36d6f6e6c1
mbabujigit <155733743+mbabujigit@users.noreply.github.com>
2024-02-03 08:07:54 -0500

    Javascript practice for loop updated Version

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var arr = [0, 1, false, true, "a", `b`, null, NaN, undefined];
-var output = [];
-for (var i = 0; i < arr.length; i++) {
-    //console.log(arr[i]);
-    if (arr[i]) {
-        output.push(arr[i]); // output = arr[i]
-    }
-}
-console.log(output) ;
-var output2 = [];
-var name="Babuji";
-console.log(name.length)
-for(var value of name){
-    console.log(value);
-    if(value){
-        output2.push(value);
-    }
+var name1 = "Babuji";
+console.log(name1);
+
+var name2 = "   Babuji    ";
+console.log(name2.trim());
+
+var name = 20;
+
+if (name) {
+    name.split();    
+    console.log(name.split(""));
 }
 
-console.log(output2) ;
+
+var a = "10";
+console.log(a);
+console.log(Number(a));
+
+
+var e = 3453533532535359.7342;
+console.log(e.toLocaleString("en-CA"));
+
+
